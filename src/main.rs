9ba2661df1ea62947ecbb4d83287f66fd062d24b
bulbanaut <aleks13102004@gmail.com>
2022-10-14 13:53:30 +0500

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::io::stdin;
+
+/*
+TODO: добавить комментарии
+*/
+
 fn main() {
-    println!("Hello, world!");
+    println!("Введите год");
+    let year: i32 = read_var();
+    if year % 4 == 0 {
+        println!("Год {year} високосный");
+    } else {
+        println!("Год {year} не високосный");
+    }
+    pause();
+}
+
+fn read_var() -> i32 {
+    loop {
+        let mut x =String::new();
+        stdin()
+            .read_line(&mut x)
+            .expect("Ошибка чтения ввода"); //считывание ввода и запись его в переменную X (с точки зрения компилятора это одна строка)
+
+        let x: i32 = match x.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Переменная Х должна быть равна числу");
+                continue;
+            },
+        }; //преобразование ввода со string в float-point integer с перезапуском loop в случае ошибки
+        break x;
+    }
+}
+
+fn pause() { //фукция паузы
+    println!("нажмите Enter чтобы выйти.");
+    let mut q = String::new();
+    stdin().read_line(&mut q).expect("ошибка");
 }
